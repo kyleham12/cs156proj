2a5936fb1399664867ccd9af2134b89b1b0c0c7c
Kyle Hamilton <fremontkdh@gmail.com>
2024-05-13 10:13:14 -0700

    feedback-based model implemented

--- app.py
+++ app.py
@@ -1,23 +1,19 @@
 import pickle
-import streamlit as st
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
-
-from sklearn.svm import SVC
-from sklearn.datasets import make_classification
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
-from sklearn.model_selection import cross_val_score
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import classification_report
+from sklearn.svm import SVC
 from flask import Flask, jsonify, request, render_template, redirect, session, url_for, send_from_directory
 from flask_cors import CORS
-from flask_sqlalchemy import SQLAlchemy
-import random
+from scipy.sparse import hstack, csr_matrix
+import numpy as np
 import os
-
 app = Flask(__name__)
 app.secret_key = 'your_secret_key_here'
 CORS(app)
+
+num_of_features = 19914
 
 CLIENT_ID = "70a9fb89662f4dac8d07321b259eaad7"
 CLIENT_SECRET = "4d6710460d764fbbb8d8753dc094d131"
@@ -26,7 +22,6 @@ CLIENT_SECRET = "4d6710460d764fbbb8d8753dc094d131"
 client_credentials_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-
 def get_song_album_cover_url(song_name, artist_name):
     search_query = f"track:{song_name} artist:{artist_name}"
     results = sp.search(q=search_query, type="track")
@@ -34,47 +29,87 @@ def get_song_album_cover_url(song_name, artist_name):
     if results and results["tracks"]["items"]:
         track = results["tracks"]["items"][0]
         album_cover_url = track["album"]["images"][0]["url"]
-        # print(album_cover_url)
+        #print(album_cover_url)
         return album_cover_url
     else:
         return "https://i.postimg.cc/0QNxYz4V/social.png"
 
+def recommend(song, svc):
+    idx = music[music['song'] == song].index[0]
+    distances = sorted(list(enumerate(similarity[idx])),reverse=True,key=lambda x:x[1])
+    recs = []
+    song_probs = []
+    
+    for m_id in distances[1:21]:
+        artist = music.iloc[m_id[0]]['artist']
+        song = music.iloc[m_id[0]]['song']
+        text = music.iloc[m_id[0]]['text']
+        
+        combo = f"{artist} {song} {text}"
+        X = tfidvector.fit_transform([combo])
 
-def recommend(song):
-    index = music[music['song'] == song].index[0]
-    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+        num_columns_to_add = num_of_features - X.shape[1]
+
+        zeros_matrix = csr_matrix((X.shape[0], num_columns_to_add), dtype=np.float64)
+
+        X = hstack([X, zeros_matrix])
+    
+        probability = svc.predict_proba(X)[0][1]
+        song_probs.append(probability)
+    
+    sorted_indices = sorted(range(len(song_probs)), key=lambda i: song_probs[i], reverse=True)
+    sorted_songs = [distances[i+1][0] for i in sorted_indices]
+    
+    recs = [music.iloc[i]['song'] for i in sorted_songs]
     recommended_music_names = []
     recommended_music_posters = []
     recommended_music_artists = []
     for i in distances[1:11]:
         artist = music.iloc[i[0]].artist
-        # print(artist)
-        # print(music.iloc[i[0]].song)
         recommended_music_posters.append(get_song_album_cover_url(music.iloc[i[0]].song, artist))
         recommended_music_names.append(music.iloc[i[0]].song)
         recommended_music_artists.append(music.iloc[i[0]].artist)
-    return recommended_music_names, recommended_music_posters, recommended_music_artists
+    name_index_map = {name: index for index, name in enumerate(recs)}
+    sorted_recs = sorted(recommended_music_names, key=lambda x: name_index_map.get(x, len(recs)))
+    sorted_artists = []
+    sorted_posters = []
+    for song in sorted_recs:
+        artist = music.loc[music['song'] == song, 'artist'].iloc[0]
+        sorted_artists.append(artist)
+        sorted_posters.append(get_song_album_cover_url(song, artist))
+    return sorted_recs,sorted_posters,sorted_artists
 
+def update_model(return_info):
+    for row in return_info:
+        song_name = row[0]
+        feedback_value = row[2]
+        music.loc[music['song'] == song_name, 'feedback'] = feedback_value
 
-# Function to update feedback based on like/dislike
-def update_feedback(song_name, liked):
-    index = music.index[music['song'] == song_name].tolist()[0]
-    if liked:
-        music.loc[index, 'feedback'] += 1
-    else:
-        music.loc[index, 'feedback'] -= 1
+    music['combined_text'] = music['artist'] + ' ' + music['song'] + ' ' + music['text']
+    tfid_X = tfidvector.fit_transform(music['combined_text'])
 
+    y = music['feedback']
 
-music = pickle.load(open('df.pkl', 'rb'))
-similarity = pickle.load(open('similarity.pkl', 'rb'))
+    X_train, X_test, y_train, y_test = train_test_split(tfid_X, y, test_size=0.2, random_state=42)
 
-music_list = music['song'].values
+    svc = SVC(probability=True)
+    svc.fit(X_train, y_train)
 
+    return svc
+
+music = pickle.load(open('df.pkl','rb'))
+similarity = pickle.load(open('similarity.pkl','rb'))
+svc = pickle.load(open('svc.pkl','rb'))
+
+tfidvector = TfidfVectorizer(analyzer='word',stop_words='english')
+
+#music_list = music['song'].values
+#artist_list = music['artist'].values
+#print(image_list = music['link'].values.tolist())
 
 @app.route("/")
 def base():
     return send_from_directory('client/build', 'index.html')
-
 
 @app.route("/<path:path>")
 def assets(path):
@@ -83,88 +118,36 @@ def assets(path):
         return response
     except:
         return send_from_directory('client/build', 'index.html')
-
-
+    
 @app.route("/getSongsList")
 def getSongsList():
-    song_list = music_list.tolist()
-    return jsonify({'songList': song_list})
-
+    song_list = music['song'].values.tolist()
+    artist_list = music['artist'].values.tolist()
+    image_list = []
+    """
+        for i in range(len(song_list)):
+        album_cover_url = get_song_album_cover_url(music.song_list[i], artist_list[i])
+        if album_cover_url is None:
+            print(f"No album cover URL found for '{song_list[i]}' by '{artist_list[i]}'")
+        image_list.append(album_cover_url)
+    """
+   
+    return jsonify({'songList': song_list, 'artist_list': artist_list, 'image_list': image_list})
 
 @app.route("/getRecList", methods=['POST'])
 def getRecList():
     data = request.get_json()
     selected_movie = data.get('selected_song')
-    recommended_music_names, recommended_music_posters, recommended_music_artists = recommend(selected_movie)
+    recommended_music_names,recommended_music_posters,recommended_music_artists = recommend(selected_movie, svc)
     recList = [];
-    for i in range(0, 10):
+    for i in range(0,10):
         temp = []
         temp.append(recommended_music_names[i])
         temp.append(recommended_music_artists[i])
         temp.append(recommended_music_posters[i])
         recList.append(temp)
-        # print(recList)
+        #print(recList)
     return jsonify({'recList': recList})
-
-
-x, y = make_classification(n_samples=5000, n_features=2,
-                           n_classes=3,
-                           n_clusters_per_class=1)
-
-xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size=0.15)
-svc = SVC()
-print(svc)
-
-SVC(C=1.0, break_ties=False, cache_size=200, class_weight=None, coef0=0.0,
-        decision_function_shape='ovr', degree=3, gamma='scale', kernel='rbf',
-        max_iter=-1, probability=False, random_state=None, shrinking=True,
-        tol=0.001, verbose=False)
-
-svc.fit(xtrain, ytrain)
-score = svc.score(xtrain, ytrain)
-print("Score: ", score)
-
-# Cross validate
-cv_scores = cross_val_score(svc, xtrain, ytrain, cv=10)
-print("CV average score: %.2f" % cv_scores.mean())
-
-# Predicting accuracy
-ypred = svc.predict(xtest)
-
-cm = confusion_matrix(ytest, ypred)
-print(cm)
-
-# Print classification report
-cr = classification_report(ytest, ypred)
-print(cr)
-
-# Iris Dataset
-
-# print("Iris dataset classification with SVC")
-data = request.get_json()
-return_info = data.get('returnInfo')
-# print("sepal length, sepal width, petal length, and petal width")
-print(return_info)
-x, y = return_info.data, return_info.target
-xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size=0.15)
-
-svc = SVC()
-print(svc)
-
-svc.fit(xtrain, ytrain)
-score = svc.score(xtrain, ytrain)
-print("Score: ", score)
-
-cv_scores = cross_val_score(svc, xtrain, ytrain, cv=10)
-print("CV average score: %.2f" % cv_scores.mean())
-
-ypred = svc.predict(xtest)
-
-cm = confusion_matrix(ytest, ypred)
-print(cm)
-
-cr = classification_report(ytest, ypred)
-print(cr)
 
 @app.route("/getOpinion", methods=['POST'])
 def getOpinion():
@@ -173,12 +156,8 @@ def getOpinion():
     return_info = data.get('returnInfo')
     print(selected_movie)
     print(return_info)
+    svc = update_model(return_info)
     return jsonify({'msg': "Opinion saved successfully"})
-
-    # Split the data
-    #xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size=0.15)
-
-    # Train the model
 
 
 if __name__ == '__main__':
